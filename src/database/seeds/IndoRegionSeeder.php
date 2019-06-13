64c500a4c78ac8b60b412fd11698de2be46ab5c9
Azis Hapidin <azishapidin@gmail.com>
2019-06-13 21:11:32 +0700

    + add global indoregion seeder

---
+++ src/database/seeds/IndoRegionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(IndoRegionProvinceSeeder::class);
+        $this->call(IndoRegionRegencySeeder::class);
+        $this->call(IndoRegionDistrictSeeder::class);
+        $this->call(IndoRegionVillageSeeder::class);
+    }
+}
