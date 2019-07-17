b4dbcac8aa38b9f67e72988c766e6b93b7b998c2
cacing69 <cacingworm69@gmail.com>
2019-07-18 00:52:48 +0700

    add some trait for cleaning code

--- src/database/models/Province.php
+++ src/database/models/Province.php
@@ -9,14 +9,15 @@
 
 namespace App\Models;
 
+use AzisHapidin\IndoRegion\Traits\ProviceTrait;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Regency;
 
 /**
  * Province Model.
  */
 class Province extends Model
 {
+    use ProviceTrait;
     /**
      * Table name.
      *
@@ -32,82 +33,5 @@ class Province extends Model
     public function regencies()
     {
         return $this->hasMany(Regency::class);
-    }
-
-
-    /**
-     * Province has many districts.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
-     */
-    public function districts()
-    {
-        return $this->hasManyThrough(District::class, Regency::class);
-    }
-
-    /**
-     * check if province has districts by name.
-     *
-     * @param string|array $name District name or array of district names.
-     * @param bool $requireAll All district in the array are required.
-     *
-     * @return bool
-     */
-    public function hasDistrictName($name, $requireAll = false)
-    {
-        if (is_array($name)) {
-            foreach ($name as $districtName) {
-                $hasDistrict = $this->hasDistrictName(strtoupper($districtName));
-                if ($hasDistrict && !$requireAll) {
-                    return true;
-                } elseif (!$hasDistrict && $requireAll) {
-                    return false;
-                }
-            }
-
-            // If we've made it this far and $requireAll is FALSE, then NONE of the districts were found
-            // If we've made it this far and $requireAll is TRUE, then ALL of the districts were found.
-            // Return the value of $requireAll;
-            return $requireAll;
-        } else {
-            $getDistrictName = array_column($this->districts->toArray(), "name");
-            if (in_array(strtoupper($name), $getDistrictName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * check if province has districts by ID.
-     *
-     * @param string|array $name District name or array of district names.
-     * @param bool $requireAll All district in the array are required.
-     *
-     * @return bool
-     */
-    public function hasDistrictId($id, $requireAll = false)
-    {
-        if (is_array($id)) {
-            foreach ($id as $districtId) {
-                $hasDistrict = $this->hasDistrictId($districtId);
-                if ($hasDistrict && !$requireAll) {
-                    return true;
-                } elseif (!$hasDistrict && $requireAll) {
-                    return false;
-                }
-            }
-
-            // If we've made it this far and $requireAll is FALSE, then NONE of the districts were found
-            // If we've made it this far and $requireAll is TRUE, then ALL of the districts were found.
-            // Return the value of $requireAll;
-            return $requireAll;
-        } else {
-            $getDistrictId = array_column($this->districts->toArray(), "id");
-            if (in_array(strtoupper($id), $getDistrictId)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- src/database/models/Regency.php
+++ src/database/models/Regency.php
@@ -9,15 +9,16 @@
 
 namespace App\Models;
 
+use AzisHapidin\IndoRegion\Traits\RegencyTrait;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Province;
-use App\Models\District;
 
 /**
  * Regency Model.
  */
 class Regency extends Model
 {
+    use RegencyTrait;
+
     /**
      * Table name.
      *
@@ -52,81 +53,5 @@ class Regency extends Model
     public function districts()
     {
         return $this->hasMany(District::class);
-    }
-
-    /**
-     * regency has many villages.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
-     */
-    public function villages()
-    {
-        return $this->hasManyThrough(Village::class, District::class);
-    }
-
-    /**
-     * check if regency has villages by name.
-     *
-     * @param string|array $name villages name or array of villages names.
-     * @param bool $requireAll All villages in the array are required.
-     *
-     * @return bool
-     */
-    public function hasVillageName($name, $requireAll = false)
-    {
-        if (is_array($name)) {
-            foreach ($name as $villageName) {
-                $hasVillage = $this->hasVillageName(strtoupper($villageName));
-                if ($hasVillage && !$requireAll) {
-                    return true;
-                } elseif (!$hasVillage && $requireAll) {
-                    return false;
-                }
-            }
-
-            // If we've made it this far and $requireAll is FALSE, then NONE of the villages were found
-            // If we've made it this far and $requireAll is TRUE, then ALL of the villages were found.
-            // Return the value of $requireAll;
-            return $requireAll;
-        } else {
-            $getVillageName = array_column($this->villages->toArray(), "name");
-            if (in_array(strtoupper($name), $getVillageName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * check if regency has villages by ID.
-     *
-     * @param string|array $name Villages name or array of villages names.
-     * @param bool $requireAll All villages in the array are required.
-     *
-     * @return bool
-     */
-    public function hasVillageId($id, $requireAll = false)
-    {
-        if (is_array($id)) {
-            foreach ($id as $villageId) {
-                $hasVillage = $this->hasVillageId($villageId);
-                if ($hasVillage && !$requireAll) {
-                    return true;
-                } elseif (!$hasVillage && $requireAll) {
-                    return false;
-                }
-            }
-
-            // If we've made it this far and $requireAll is FALSE, then NONE of the villages were found
-            // If we've made it this far and $requireAll is TRUE, then ALL of the villages were found.
-            // Return the value of $requireAll;
-            return $requireAll;
-        } else {
-            $getVillageId = array_column($this->villages->toArray(), "id");
-            if (in_array(strtoupper($id), $getVillageId)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
