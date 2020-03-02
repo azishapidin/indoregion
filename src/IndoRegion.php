159925a2fac6b07c73e9c1d2ca97ab1746e39ecc
Azis Hapidin <azishapidin@gmail.com>
2020-03-02 16:24:15 +0700

    + tmp class

---
+++ src/IndoRegion.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the IndoRegion package.
+ *
+ * (c) Azis Hapidin <azishapidin.com | azishapidin@gmail.com>
+ *
+ */
+
+namespace AzisHapidin\IndoRegion;
+
+use App\Models\Province;
+use App\Models\Regency;
+use App\Models\District;
+use App\Models\Village;
+
+class IndoRegion
+{
+    public function getProvinces()
+    {
+        # code...
+    }
+
+    public function searchProvinces($name = '')
+    {
+        # code...
+    }
+
+    public function findProvince($key = '', $value = '')
+    {
+        # code...
+    }
+}
