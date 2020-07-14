92d7895ecd024d0e893e797d93258291a4dac80b
cacing69 <cacingworm69@gmail.com>
2020-07-14 16:35:09 +0700

    add parameter only in populate command

--- src/Commands/PupolateCommand.php
+++ src/Commands/PupolateCommand.php
@@ -18,7 +18,7 @@ class PupolateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'indoregion:populate';
+    protected $signature = 'indoregion:populate {--only=}';
 
     /**
      * The console command description.
@@ -26,6 +26,13 @@ class PupolateCommand extends Command
      * @var string
      */
     protected $description = 'Migration & Seed from IndoRegion';
+
+    protected $mapSeeder = [
+        "province" => "IndoRegionProvinceSeeder",
+        "regency" => "IndoRegionRegencySeeder",
+        "district" => "IndoRegionDistrictSeeder",
+        "village" => "IndoRegionVillageSeeder",
+    ];
 
     /**
      * Compatiblity for Lumen 5.5.
@@ -45,6 +52,8 @@ class PupolateCommand extends Command
     public function fire()
     {
 
+        $this->validateOnly();
+
         if ($this->confirm("Proceed tables & rows creation? [yes|no]", "yes")) {
 
             $this->line('');
@@ -53,15 +62,57 @@ class PupolateCommand extends Command
             Artisan::call('migrate');
             $this->line('');
             $this->info("seeding files...");
-            Artisan::call('db:seed', [
-                "--class" => "IndoRegionSeeder"
-            ]);
-            $this->line('');
-            $this->info("Pupolate IndoRegion complete");
 
+            if($this->validateOnly()) {
+                foreach ($this->getOnlyAsArray() as $k => $v) {
+                    $this->info("Seed {$this->getSeeder($v)}");
+                    Artisan::call('db:seed', [
+                        "--class" => $this->getSeeder($v)
+                    ]);
+                }
+                
+                $this->info("Pupolate complete");
+            } else {
+                Artisan::call('db:seed', [
+                    "--class" => "IndoRegionSeeder"
+                ]);
+
+                $this->info("Pupolate IndoRegion complete");
+            }
+            
             $this->line('');
         }
+    }
 
-        
+    private function getOnlyAsArray()
+    {
+        return explode(",", $this->option('only'));
+    }
+
+    private function validateOnly()
+    {
+        $return = false;
+
+        if(!empty($this->option("only"))) {
+            foreach ($this->getOnlyAsArray() as $v) {
+                if(!array_key_exists($v, $this->mapSeeder)) {
+                    throw new \Exception("parameter not valid, be sure to use : {$this->getValidKeyAsString()}");
+                }
+            }
+
+            $return = true;
+        }
+
+        return $return;
+    }
+
+    public function getValidKeyAsString()
+    {
+        return implode(",", array_keys($this->mapSeeder));
+    }
+
+    public function getSeeder($key)
+    {
+        return $this->mapSeeder[$key];
     }
 }
